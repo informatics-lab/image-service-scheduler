a02dfe191359169a6e86cafb26f3ced126d5f1b0
Niall Robinson <niall.h.robinson@gmail.com>
2015-07-29 12:07:15 +0100

    more work on queuing

--- scheduler.py
+++ scheduler.py
@@ -15,6 +15,7 @@ class Job(object):
         self.model = self.data_file.split("_")[0]
         self.variable = "_".join(self.data_file.split("_")[1:-1])
         self.timestamp = self.data_file.split("_")[-1].split(".")[0]
+        self.message = message
     
     def __str__(self):
         self.data_file
@@ -27,10 +28,13 @@ class Job(object):
         profs = config.getProfiles(self.model, self.variable)
         msgs = []
         for prof in profs:
-            for time in self.getTimes():
+            times = self.getTimes()
+            for i, time in enumerate(times):
                 msg = {"data_file": self.data_file,
-                                  "profile": prof,
-                                  "time_step": time}
+                       "profile": prof,
+                       "time_step": time,
+                       "frame": i,
+                       "nframes": len(times)}
                 msgs.append(msg)
 
         return msgs
@@ -43,11 +47,15 @@ class NoJobsError(Exception):
         return repr(self.value)
 
 
-def getTHREDDSJob(queue_name="thredds_queue", visibility_timeout=60):
+def getQueue(queue_name):
     conn = boto.sqs.connect_to_region(os.getenv("AWS_REGION"),
                                       aws_access_key_id=os.getenv("AWS_KEY"),
                                       aws_secret_access_key=os.getenv("AWS_SECRET_KEY"))
     queue = conn.get_queue(queue_name)
+    return queue
+
+
+def getTHREDDSJob(queue, visibility_timeout=60):
     messages = queue.get_messages(visibility_timeout, number_messages=1)
     try:
         message = messages[0]
@@ -60,12 +68,9 @@ def getTHREDDSJob(queue_name="thredds_queue", visibility_timeout=60):
     return job
 
 
-def postImgSvcJobs(msgs, queue_name="image_service_queue"):
-    conn = boto.sqs.connect_to_region(os.getenv("AWS_REGION"),
-                                      aws_access_key_id=os.getenv("AWS_KEY"),
-                                      aws_secret_access_key=os.getenv("AWS_SECRET_KEY"))
-    queue = conn.get_queue(queue_name)
-    for msg in msgs:
+def postImgSvcJobs(msgs, queue):
+    nframes = len(msgs)
+    for i, msg in enumerate(msgs):
         print "Adding " + msg + " to the img svc job queue"
         m = boto.sqs.message.Message()
         m.message_attributes(msg)
@@ -73,6 +78,9 @@ def postImgSvcJobs(msgs, queue_name="image_service_queue"):
 
 
 if __name__ == "__main__":
-    job = getTHREDDSJob()
-    print "Picked up " + job
-    postImgSvcJobs(job.getImgSvcJobMsgs())
+    thredds_queue = getQueue("thredds_queue")
+    image_service_queue = getQueue("image_service_queue")
+
+    job = getTHREDDSJob(thredds_queue)
+
+    postImgSvcJobs(job.getImgSvcJobMsgs(), image_service_queue)
